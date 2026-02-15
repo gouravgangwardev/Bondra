703bdbd1039bed820cab39e0ac7ad5ee6dcebc97
gouravgangwardev <gangwargourav24@gmail.com>
2026-02-15 22:46:07 +0530

    Create index.ts

---
+++ frontend/src/components/layout/index.ts
@@ -0,0 +1,5 @@
+// src/components/layout/index.ts
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export type { User } from './Navbar';
+export { default as Sidebar } from './Sidebar';
